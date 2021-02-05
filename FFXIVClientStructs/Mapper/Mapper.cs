db8235e6dd64b7e16329f9fdc47fea8a8f5c0c55
Raymond Lynch <daemitus@gmail.com>
2021-02-05 12:51:34 -0500

    More work on mapper

--- FFXIVClientStructs/Mapper/Mapper.cs
+++ FFXIVClientStructs/Mapper/Mapper.cs
@@ -1,31 +1,103 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Reflection;
+using YamlDotNet.Serialization;
 
 namespace FFXIVClientStructs.Mapper
 {
     public static class Mapper
     {
-        private static readonly long ProgramBaseAddress;
+        private static readonly long ProgramBaseAddress = Process.GetCurrentProcess().MainModule.BaseAddress.ToInt64();
         private const long BaseAddress = 0x140000000;
 
         static Mapper()
         {
-            var targetModule = Process.GetCurrentProcess().MainModule;
-            ProgramBaseAddress = targetModule.BaseAddress.ToInt64();
-
-            UpdateGlobals();
-            UpdateFunctions();
-            UpdateClasses();
+            var gameVersion = GetCurrentVersion();
+            if (gameVersion != Game.Version)
+            {
+                var data = GetLocalData();
+                if (data == null || data.Version != gameVersion)
+                {
+                    DownloadData();
+                    data = GetLocalData();
+                }
+                if (data == null || data.Version != gameVersion)
+                {
+                    UpdateAddresses(ZeroOutAddress);
+                }
+                else
+                {
+                    UpdateAddresses(CalculateAddress);
+                }
+            }
+            else
+            {
+                UpdateAddresses(CalculateAddress);
+            }
         }
 
-        private static void UpdateGlobals() => UpdateSimple(typeof(Globals));
+        private static string GetCurrentVersion()
+        {
+            var filePath = Process.GetCurrentProcess().MainModule.FileName;
+            var gameDir = Path.GetDirectoryName(filePath);
+            var versionPath = Path.Combine(gameDir, "ffxivgame.ver");
+            var version = File.ReadAllText(versionPath).Trim();
+            return version;
+        }
 
-        private static void UpdateFunctions() => UpdateSimple(typeof(Functions));
+        private static DataConstruct GetLocalData()
+        {
+            var dllPath = Assembly.GetExecutingAssembly().Location;
+            var dllDir = Path.GetDirectoryName(dllPath);
+            var dataPath = Path.Combine(dllDir, "data.yml");
+            if (!File.Exists(dataPath))
+                return null;
 
-        private static void UpdateClasses() { }
+            var dataText = File.ReadAllText(dataPath);
+            var deserializer = new DeserializerBuilder().Build();
+            var data = deserializer.Deserialize<DataConstruct>(dataText);
+            return data;
+        }
 
-        private static void UpdateSimple(Type staticType)
+        private static void DownloadData()
+        {
+            var dllPath = Assembly.GetExecutingAssembly().Location;
+            var dllDir = Path.GetDirectoryName(dllPath);
+            var dataPath = Path.Combine(dllDir, "data.yml");
+
+            var web = new WebClient();  // TODO: Error checking
+            web.DownloadFile("https://raw.githubusercontent.com/aers/FFXIVClientStructs/main/ida/data.yml", dataPath);
+        }
+
+        private static void UpdateAddresses(Func<long, IntPtr> getNewValue)
+        {
+            UpdateGlobals(getNewValue);
+            UpdateFunctions(getNewValue);
+            UpdateClasses(getNewValue);
+        }
+
+        private static void UpdateGlobals(Func<long, IntPtr> getNewValue) => UpdateSimple(typeof(Globals), getNewValue);
+
+        private static void UpdateFunctions(Func<long, IntPtr> getNewValue) => UpdateSimple(typeof(Functions), getNewValue);
+
+        private static void UpdateClasses(Func<long, IntPtr> getNewValue)
+        {
+            var nestedTypes = typeof(Classes).GetNestedTypes(BindingFlags.Public | BindingFlags.Static);
+            foreach (var nestedType in nestedTypes)
+            {
+                if (!nestedType.IsClass)
+                    continue;
+
+                if (nestedType.GetCustomAttribute<DataNameAttribute>() == null)
+                    continue;
+
+                UpdateSimple(nestedType, getNewValue);
+            }
+        }
+
+        private static void UpdateSimple(Type staticType, Func<long, IntPtr> getNewValue)
         {
             var propInfos = staticType.GetProperties(BindingFlags.Public | BindingFlags.Static);
             foreach (var propInfo in propInfos)
@@ -33,19 +105,26 @@ namespace FFXIVClientStructs.Mapper
                 if (propInfo.PropertyType != typeof(IntPtr))
                     continue;
 
-                var currentValue = (IntPtr)propInfo.GetValue(null);  // Because static classes cannot be instanced
-                propInfo.SetValue(null, CalculateAddress(currentValue.ToInt64()));
+                if (propInfo.GetCustomAttribute<DataNameAttribute>() == null)
+                    continue;
+
+                var currentValue = (IntPtr)propInfo.GetValue(null);
+                propInfo.SetValue(null, getNewValue(currentValue.ToInt64()));
             }
         }
 
-        private static IntPtr CalculateAddress(long address)
-        {
-            if (ProgramBaseAddress == 0)
-                throw new InvalidOperationException("Mapper is not initialized");
+        private static IntPtr CalculateAddress(long address) => new IntPtr(address - BaseAddress + ProgramBaseAddress);
 
-            return new IntPtr(address - BaseAddress + ProgramBaseAddress);
-        }
+        private static IntPtr ZeroOutAddress(long _) => IntPtr.Zero;
     }
+
+    public static partial class Game { }
+
+    public static partial class Globals { }
+
+    public static partial class Functions { }
+
+    public static partial class Classes { }
 
     public class DataNameAttribute : Attribute
     {
@@ -56,10 +135,4 @@ namespace FFXIVClientStructs.Mapper
             DataName = name;
         }
     }
-
-    public static partial class Globals { }
-
-    public static partial class Functions { }
-
-    public static partial class Classes { }
 }
